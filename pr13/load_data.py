524dcf12003cc8f8ac59ff5ac28208a36f16b48f
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-11-24 12:13:27 +0300

    test BeautifulSoup

--- pr13/load_data.py
+++ pr13/load_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from bs4 import BeautifulSoup
+import requests
 
 def get_students(fileName):
     df = pd.read_excel(fileName)
@@ -15,11 +16,16 @@ def get_students(fileName):
 
 
 def main():
-    list_students3 = get_students('biso03.xlsx')
-    list_students4 = get_students('biso04.xlsx')
-    list_students = list_students3 + list_students4
-    print(list_students)
+    # list_students3 = get_students('biso03.xlsx')
+    # list_students4 = get_students('biso04.xlsx')
+    # list_students = list_students3 + list_students4
+    # print(list_students)
 
-
+    url=r"https://generatefakename.com/ru/address/ru/ru"
+    url=r"https://www.mirea.ru/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page, "html.parser")
+    colleges = soup.findAll('h3')
+    print(colleges)
 if __name__ == '__main__':
     main()
